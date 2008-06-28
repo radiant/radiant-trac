a1ab134f71cb5ef521cbe4d16fdcd71bd9d75155
John W. Long <me@johnwlong.com>
2008-06-27 21:43:25 -0400

    added AVLUX to the footer

--- site_footer.cs
+++ site_footer.cs
@@ -4,7 +4,9 @@
 ?>
 <div id="foot">
   <p>
-    Web Site Design, Logo, Etc. Copyright &copy; 2006, <a href="http://wiseheartdesign.com/">John W. Long</a>. All Rights Reserved.
+    Web Site Design, Logo, Etc. Copyright &copy; 2006&#8211;2008, <a href="http://wiseheartdesign.com/">John W. Long</a>. All Rights Reserved.
+    <br />
+    Hosting is graciously provided by the kind folks at <a href="http://avlux.net">AVLUX</a>.
   </p>
 </div>
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
